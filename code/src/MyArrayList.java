61797e612769ab3d69f12c4f018abf5acd348afa
shadyman <m.m.shdmn@gmail.com>
2023-04-18 20:27:24 +0600

    contains method

--- code/src/MyArrayList.java
+++ code/src/MyArrayList.java
@@ -39,7 +39,12 @@ public class MyArrayList<T> implements MyArray{
 
     @Override
     public boolean contains(Object o) {
-
+        for(int i = 0; i < size; i++){
+            if (arr[i].equals(o)){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
