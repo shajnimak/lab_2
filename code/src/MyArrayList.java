eb17c13571ad9a01834da9936a6dc2f1066769d5
shadyman <m.m.shdmn@gmail.com>
2023-04-18 20:26:49 +0600

    size method

---
+++ code/src/MyArrayList.java
@@ -0,0 +1,102 @@
+import repository.MyArray;
+
+public class MyArrayList<T> implements MyArray{
+    private T[] arr;
+    private int size;
+
+    MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+//    public void  add(T element){
+//        if(size == arr.length){
+//            increaseBuffer();
+//        }
+//        arr[size++] = element;
+//    }
+//
+//    public void increaseBuffer(){
+//        T[] newArr = (T[]) new Object[arr.length*2];
+//        for(int i=0; i< arr.length; i++){
+//            newArr[i]=arr[i];
+//        }
+//        arr = newArr;
+//    }
+//
+//    public T getElement(int index) {
+//        checkIndex(index);
+//        return arr[index];
+//    }
+//
+//    public int getSize() {
+//        return size;
+//    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    public void clear(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    public void delete(int index){
+        checkIndex(index);
+        for(int i= index + 1; i<size; i++){
+            arr[i-1] = arr[i];
+        }
+        size--;
+    }
+    public void checkIndex(int index){
+        if(index < 0 || index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
