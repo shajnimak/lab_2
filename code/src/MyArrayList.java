481c36868b9c41d297b04e0ee0b600c538398039
shadyman <m.m.shdmn@gmail.com>
2023-04-19 20:44:50 +0600

    indexOf method

--- code/src/MyArrayList.java
+++ code/src/MyArrayList.java
@@ -96,7 +96,14 @@ public class MyArrayList<T> implements MyArray{
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(o)) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 
     @Override
