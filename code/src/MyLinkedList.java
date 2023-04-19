5df9f114055147d1d8aab27933de1b91fcbf9f6e
shadyman <m.m.shdmn@gmail.com>
2023-04-19 21:47:30 +0600

    first commit

---
+++ code/src/MyLinkedList.java
@@ -0,0 +1,81 @@
+import repository.MyArray;
+
+public class MyLinkedList<T> implements MyArray {
+    private class Node<T> {
+        T var;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(T val, Node<T> next, Node<T> prev) {
+            this.var = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        size = 0;
+    }
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+
+}
