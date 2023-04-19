43277f0999587f0f5c50b189a4e82b5243f3050e
shadyman <m.m.shdmn@gmail.com>
2023-04-19 21:52:44 +0600

    remove method

--- code/src/MyLinkedList.java
+++ code/src/MyLinkedList.java
@@ -81,6 +81,25 @@ public class MyLinkedList<T> implements MyArray {
 
     @Override
     public boolean remove(Object item) {
+        Node<T> newNode = new Node<T>((T) item, null, null);
+        if (head.var == newNode.var) {
+            head = head.next;
+            head.prev = null;
+            size--;
+            return true;
+        }
+        Node<T> ptr = head.next;
+        while (ptr != null) {
+            if (ptr.var == newNode.var) {
+                Node<T> temp = ptr.prev;
+                temp.next = ptr.next;
+                Node<T> temp2 = ptr.next;
+                temp2.prev = temp;
+                size--;
+                return true;
+            }
+            ptr = ptr.next;
+        }
         return false;
     }
 
