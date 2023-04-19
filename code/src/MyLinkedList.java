cf79035a02234b8852d5a6b084fb582ce05afe4d
shadyman <m.m.shdmn@gmail.com>
2023-04-19 21:49:12 +0600

    add method

--- code/src/MyLinkedList.java
+++ code/src/MyLinkedList.java
@@ -44,7 +44,17 @@ public class MyLinkedList<T> implements MyArray {
 
     @Override
     public void add(Object item) {
-
+        Node<T> newNode = new Node<T>((T) item, null, null);
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        }
+        else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
